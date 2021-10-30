f3440d478b228aa5bda185739bd0c787f37ea287
Kenny Putman <kbputman@gmail.com>
2021-10-30 08:54:34 -0500

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+import data from "./data.json";
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+
+app.get("/all", (reqeust, response) => {
+  response.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
